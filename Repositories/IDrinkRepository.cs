f6e176f0841609c2af3db469b1e9c04162da3498
Alex Fun <alexanderthefun91@gmail.com>
2023-06-11 23:43:16 -0500

    broths and drinks done. Client Folder and react app created, next will be making the manager modules

---
+++ Repositories/IDrinkRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShabuShabu.Models;
+using System.Collections.Generic;
+
+namespace ShabuShabu.Repositories
+{
+	public interface IDrinkRepository
+	{
+		Drink AddDrink(Drink drink);
+		void DeleteDrink(int id);
+		List<Drink> GetAllDrinks();
+		void UpdateDrink(Drink drink);
+	}
+}
